129107835dd58e2e814c0d426c88299f7d525688
Andrew Chin <achin@localhost.localdomain>
2021-06-07 01:33:18 -0400

    Add built headers

---
+++ headers/scripts/mod/.empty.o.cmd
@@ -0,0 +1,8 @@
+cmd_scripts/mod/empty.o := gcc -Wp,-MD,scripts/mod/.empty.o.d  -nostdinc -isystem /usr/lib/gcc/i586-linux-gnu/4.9/include -Iinclude  -I/home/achin/Documents/build-kernel-headers/source/linux-2.6.32/arch/x86/include -include include/linux/autoconf.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -Os -m32 -msoft-float -mregparm=3 -freg-struct-return -mpreferred-stack-boundary=2 -march=atom -mtune=atom -mtune=generic -Wa,-mtune=generic32 -ffreestanding -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -Wframe-larger-than=2048 -fno-stack-protector -fno-omit-frame-pointer -fno-optimize-sibling-calls -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-dwarf2-cfi-asm -fconserve-stack   -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(empty)"  -D"KBUILD_MODNAME=KBUILD_STR(empty)"  -c -o scripts/mod/empty.o scripts/mod/empty.c
+
+deps_scripts/mod/empty.o := \
+  scripts/mod/empty.c \
+
+scripts/mod/empty.o: $(deps_scripts/mod/empty.o)
+
+$(deps_scripts/mod/empty.o):
