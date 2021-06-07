bdbe9b9444cbb5c1499d20395e2c0af1ad8a3fe4
Andrew Chin <achin@localhost.localdomain>
2021-06-07 01:23:37 -0400

    Fix script

--- build_headers.sh
+++ build_headers.sh
@@ -2,22 +2,25 @@
 # WARNING! Incomplete script. Just to get the gist of it
 
 # Decompress the source
-tar -xzf source/linux-2.6.32.tar.gz
+echo "Decompressing kernel tree"
+tar -xzf source/linux-2.6.32.tar.gz -C source
 
 # Copy the config to the linux source tree
 cp config/atom_i386_defconfig source/linux-2.6.32/.config
 
 # Prepare kernel source
 pushd source/linux-2.6.32
+echo "Preparing kernel source"
 make prepare
 make scripts
 
 # Build kernel headers (see 4.4.111 /scripts/package/builddeb)
-find . \( -name Makefile\* -o -name Kconfig\* -o -name \*.lp \) -exec cp --parent "{}" ../headers \;
-find arch/x86/include include scripts -type f -exec cp --parent "{}" ../headers \;
-find arch/x86 \( -name module.lds -o -name Kbuild.platforms -o -name Platform \) -exec cp --parent "{}" ../headers \;
-find $(find arch/x86 \( -name include -o -name scripts -type d \)) -type f -exec cp --parent "{}" ../headers \;
-find arch/x86/include Module.symvers include scripts -type f -exec --parent cp --parent "{}" ../headers \;
+echo "Building kernel headers"
+find . \( -name Makefile\* -o -name Kconfig\* -o -name \*.lp \) -exec cp --parent "{}" ../../headers \;
+find arch/x86/include include scripts -type f -exec cp --parent "{}" ../../headers \;
+find arch/x86 \( -name module.lds -o -name Kbuild.platforms -o -name Platform \) -exec cp --parent "{}" ../../headers \;
+find $(find arch/x86 \( -name include -o -name scripts -type d \)) -type f -exec cp --parent "{}" ../../headers \;
+find arch/x86/include Module.symvers include scripts -type f -exec cp --parent "{}" ../../headers \;
 
 # Finish up
 popd
