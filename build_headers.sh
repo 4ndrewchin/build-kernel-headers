dea5401dbb0caecf0a6010b88154f298fe1c3afc
Andrew Chin <achin@localhost.localdomain>
2021-06-07 22:07:46 -0400

    Fix minor bugs

--- build_headers.sh
+++ build_headers.sh
@@ -16,10 +16,10 @@ make scripts
 
 # Build kernel headers (see 4.4.111 /scripts/package/builddeb)
 echo "Building kernel headers"
-find . \( -name Makefile\* -o -name Kconfig\* -o -name \*.lp \) -exec cp --parent "{}" ../../headers \;
+find . \( -name Makefile\* -o -name Kconfig\* -o -name \*.pl \) -exec cp --parent "{}" ../../headers \;
 find arch/x86/include include scripts -type f -exec cp --parent "{}" ../../headers \;
 find arch/x86 \( -name module.lds -o -name Kbuild.platforms -o -name Platform \) -exec cp --parent "{}" ../../headers \;
-find $(find arch/x86 \( -name include -o -name scripts -type d \)) -type f -exec cp --parent "{}" ../../headers \;
+find $(find arch/x86 \( -name include -o -name scripts \) -type d) -type f -exec cp --parent "{}" ../../headers \;
 find arch/x86/include Module.symvers include scripts -type f -exec cp --parent "{}" ../../headers \;
 
 # Finish up
